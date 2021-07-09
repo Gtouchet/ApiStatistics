8c96db6190e67fef9afaff7952eb5eb3fdf4766d
GitHub Action <action@github.com>
2021-07-09 16:14:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/gtouchet/apistatistics/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/gtouchet/apistatistics/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.gtouchet.apistatistics.listeners
 
+import com.github.gtouchet.apistatistics.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/gtouchet/apistatistics/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/gtouchet/apistatistics/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.gtouchet.apistatistics.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gtouchet.apistatistics.MyBundle
 
 class MyApplicationService {
 
